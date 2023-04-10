6fc669324e4b51ac686f17a7777253ac7ad893f7
qDread-dev <58754377+qDread-dev@users.noreply.github.com>
2023-04-10 18:32:30 +0000

    ads

---
+++ js/functions.js
@@ -0,0 +1,30 @@
+function simplifySqrt(simpSqrtInput, simpSqrtOutput, k){
+    for (let i = 1; i * i <= simpSqrtInput.value; i++)
+    {
+        if (simpSqrtInput.value % (i * i) == 0)
+        {
+            k = i;
+        }
+    }
+    if (simpSqrtInput / (k * k) === null)
+    {
+        simpSqrtOutput.value = k;
+    }
+    if (simpSqrtInput.value / (k * k) == 1)
+    {
+        simpSqrtOutput.value = k;
+    }
+    else if (simpSqrtInput.value / (k * k) > 1)
+    {
+        if (k == "1")
+        {
+            simpSqrtOutput.value = "\u{221A}" + (simpSqrtInput.value / (k * k));
+        }
+        else
+        {
+            simpSqrtOutput.value = k + "\u{221A}" + (simpSqrtInput.value / (k * k));
+        }
+    }
+};
+
+export { simplifySqrt};
