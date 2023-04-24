e337d5bbcb008b8d0f091809d608eaa7149ec5ae
qDread-dev <58754377+qDread-dev@users.noreply.github.com>
2023-04-24 12:50:14 +0000

    Removed old simplify square root, hope nothing breaks

--- js/functions.js
+++ js/functions.js
@@ -14,34 +14,6 @@ function changeTitle(title){
 
 
 // Below are the functions for the calculators
-function simplifySqrt(simpSqrtInput, simpSqrtOutput, k){
-    for (let i = 1; i * i <= simpSqrtInput.value; i++)
-    {
-        if (simpSqrtInput.value % (i * i) == 0)
-        {
-            k = i;
-        }
-    }
-    if (simpSqrtInput / (k * k) === null)
-    {
-        simpSqrtOutput.innerHTML = k;
-    }
-    if (simpSqrtInput.value / (k * k) == 1)
-    {
-        simpSqrtOutput.innerHTML = k;
-    }
-    else if (simpSqrtInput.value / (k * k) > 1)
-    {
-        if (k == "1")
-        {
-            simpSqrtOutput.innerHTML = "\u{221A}" + (simpSqrtInput.value / (k * k));
-        }
-        else
-        {
-            simpSqrtOutput.innerHTML = k + "\u{221A}" + (simpSqrtInput.value / (k * k));
-        }
-    }
-};
 
 function simplifySqrtRefactor(simpSqrtInput){
     k = 1;
